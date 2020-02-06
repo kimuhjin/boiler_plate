bb98741d86ead277fa9956a6d49fbb0480f8509e
UhJin <uhjin@UhJinui-MacBookPro.local>
2020-02-06 22:20:34 +0900

    final

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const { User } = require("./models/User");
+const { auth } = require("./middleware/auth");
 const bodyParser = require("body-parser");
 const config = require("./config/key");
 const cookieParser = require("cookie-parser");
@@ -23,7 +24,7 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello worldaa"));
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
@@ -33,7 +34,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   //요청된 이메일이 DB에 있는지 검색한다.
 
   User.findOne({ email: req.body.email }, (err, user) => {
@@ -65,6 +66,30 @@ app.post("/login", (req, res) => {
           .status(200)
           .json({ loginSuccess: true, userId: user._id });
       });
+    });
+  });
+});
+
+app.get("api/users/auth", auth, (req, res) => {
+  //여기까지 미들웨어가 통과했다는 것은 Authentication이 True라는 뜻
+  res.status(200).json({
+    _id: req.user._id,
+    //role 0 -> 일반유저, role 0 이 아니면 관리자
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true
     });
   });
 });
--- models/User.js
+++ models/User.js
@@ -54,7 +54,8 @@ userSchema.pre("save", function(next) {
 
 userSchema.methods.comparePassword = function(plainPassword, cb) {
   bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
-    if (err) return cb(err), cb(null, isMatch);
+    if (err) return cb(err);
+    cb(null, isMatch);
   });
 };
 
@@ -66,6 +67,26 @@ userSchema.methods.generateToken = function(cb) {
   user.save(function(err, user) {
     if (err) return cb(err);
     cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function(token, cb) {
+  var user = this;
+
+  //토킄을 decode 한다
+  jwt.verify(token, "secretToken", function(err, decoded) {
+    //유저 아이디를 이용해서 유저를 찾은 다음에,
+    //클라이언트에서 가져온 토큰과 DB에 보관된 토큰이 일치하는지 확인
+    user.findOne(
+      {
+        _id: decoded,
+        token: token
+      },
+      function(err, user) {
+        if (err) return cb(err);
+        cb(null, user);
+      }
+    );
   });
 };
 
